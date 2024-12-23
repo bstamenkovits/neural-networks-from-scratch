9e6d17248e28af913d189f11f84aaa7d214c2fff
Bela <none_of_your_bees@wax.com>
2024-12-23 21:26:46 +0100

    docstrings

--- mpl/value.py
+++ mpl/value.py
@@ -1,4 +1,4 @@
-import math 
+import math
 
 
 class Value:
@@ -6,7 +6,7 @@ class Value:
     def __init__(self, data: float, symbol: str = None, expression: str = '', children: tuple = (), operation: str ='') -> None:
         self.data = data
         self.symbol = symbol if symbol else expression
-        self.expression = expression 
+        self.expression = expression
         self.operation = operation
         self.backwards = lambda: None
         self.gradient = 0
@@ -15,7 +15,7 @@ class Value:
 
     def __repr__(self) -> str:
         return f"Value({self.data})"
-    
+
     def get_label(self):
         if self.expression:
             label = f"{self.symbol} = {self.expression}"
@@ -24,93 +24,125 @@ class Value:
         else:
             label = self.symbol
         return label
-    
+
     def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)  # ensure other is an instance of the Value class
+        """
+        output = self + other
+        """
+        # ensure other is an instance of the Value class
+        other = other if isinstance(other, Value) else Value(other)
+
         output = Value(
-            data = self.data + other.data, 
+            data = self.data + other.data,
             expression = f"{self.symbol} + {other.symbol}" if self.symbol and other.symbol else '',
-            children = (self, other), 
+            children = (self, other),
             operation = '+'
         )
 
-        def func():
+        def backwards():
+            """
+            z = x + y
+            ∇x = ∇z * dz/dx       dz/dx = 1
+            ∇y = ∇z * dz/dy       dz/dy = 1
+            """
             self.gradient += 1.0 * output.gradient
             other.gradient += 1.0 * output.gradient
-        output.backwards = func
-        
+        output.backwards = backwards
+
         return output
-    
+
     def __sub__(self, other):
-        other = other if isinstance(other, Value) else Value(other)  # ensure other is an instance of the Value class
+        """
+        output = self - other
+        """
+        # ensure other is an instance of the Value class
+        other = other if isinstance(other, Value) else Value(other)
+
         output = Value(
-            data = self.data - other.data, 
+            data = self.data - other.data,
             expression = f"{self.symbol} - {other.symbol}" if self.symbol and other.symbol else '',
-            children = (self, other), 
+            children = (self, other),
             operation = '-'
         )
 
-        def func():
+        def backwards():
+            """
+            z = x - y
+            ∇x = ∇z * dz/dx       dz/dx =  1
+            ∇y = ∇z * dz/dy       dz/dy = -1
+            """
             self.gradient += 1.0 * output.gradient
             other.gradient += -1.0 * output.gradient
-        output.backwards = func
-        
+
+        output.backwards = backwards
         return output
-    
+
     def __mul__(self, other):
+        """
+        output = self * other
+        """
         other = other if isinstance(other, Value) else Value(other)  # ensure other is an instance of the Value class
         output = Value(
-            data = self.data * other.data, 
+            data = self.data * other.data,
             expression = f"{self.symbol} * {other.symbol}" if self.symbol and other.symbol else '',
-            children = (self, other), 
+            children = (self, other),
             operation = '*'
         )
 
-        def func():
+        def backwards():
+            """
+            z = x * y
+            ∇x = ∇z * dz/dx       dz/dx = y
+            ∇y = ∇z * dz/dy       dz/dy = x
+            """
             self.gradient += other.data * output.gradient
             other.gradient += self.data * output.gradient
-        output.backwards = func
-        
+
+        output.backwards = backwards
         return output
-    
+
     def __truediv__(self, other):
         other = other if isinstance(other, Value) else Value(other)  # ensure other is an instance of the Value class
         output = Value(
-            data = self.data / other.data, 
+            data = self.data / other.data,
             expression = f"{self.symbol} / {other.symbol}" if self.symbol and other.symbol else '',
-            children = (self, other), 
+            children = (self, other),
             operation = '/'
         )
         return output
-    
+
     def __neg__(self):
         return self * -1
-    
+
     def __radd__(self, other):
         return self + other
-    
+
     def __rsub__(self, other):
         return self - other
-    
+
     def __rmul__(self, other):
         return self * other
-    
+
     def __rtruediv__(self, other):
         return self / other
-    
+
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "only int/float powers"
         output = Value(
-            data = self.data**other, 
-            children=(self,), 
+            data = self.data**other,
+            children=(self,),
             operation=f'**{other}',
             expression=f'{self.symbol}**{self.other}' if self.symbol and other.symbol else '',
         )
 
-        def func():
+        def backwards():
+            """
+            z = x ^ y
+            ∇x = ∇z * dz/dx       dz/dx = y * x^(y-1)
+            """
             self.gradient += (other * self.data**(other-1)) * output.gradient
-        output.backwards = func
 
+        output.backwards = backwards
         return output
 
     def tanh(self):
@@ -126,8 +158,8 @@ class Value:
         def func():
             self.gradient += (1 - t**2) * output.gradient
         output.backwards = func
-        
-        return output        
+
+        return output
 
     def back_propogate(self):
         self.gradient = 1.0
@@ -139,15 +171,15 @@ class Value:
                     build_topological_graph(child, graph, visited)
                 graph.append(tree)
             return graph
-        
+
         for value in reversed(build_topological_graph(self)):
             value.backwards()
-            
+
 
 if __name__ == "__main__":
     from visualization import Visualizer
     visualizer = Visualizer()
-    
+
     a = Value(2)
     b = Value(-3)
     c = Value(10)
